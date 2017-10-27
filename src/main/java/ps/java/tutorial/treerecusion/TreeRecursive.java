b8d624c83c2b7b762d2d1414bef96e1cc6e97c84
Petr Sládek <petr.sladek@i.cz>
2017-10-27 02:47:47 +0200

    Java tutorial tree recursion

---
+++ src/main/java/ps/java/tutorial/treerecusion/TreeRecursive.java
@@ -0,0 +1,34 @@
+package ps.java.tutorial.treerecusion;
+
+import java.io.File;
+
+public class TreeRecursive {
+
+    public static void main(final String... a) {
+        final File root = new File(".");
+        tree("", "", root);
+//        tree("", "", new File("/"));
+    }
+
+    private static void tree(final String namePrefix, final String treePrefix, final File file) {
+        System.out.println(namePrefix + file.getName());
+        final File[] files = file.listFiles(File::isDirectory);
+
+        if (files == null) {
+            return;
+        }
+
+        final int length = files.length;
+
+        for (int i = 0; i < length; i++) {
+            final File f = files[i];
+
+            if (i < length - 1) {
+                tree(treePrefix + " +", treePrefix + " |", f);
+            } else {
+                tree(treePrefix + " `", treePrefix + "  ", f);
+            }
+        }
+    }
+
+}
